b5be53536a31c5a67ee8418f6cc4b6956e0c0387
Oliver Hughes <oliver.hughes@pleo.io>
2026-04-15 18:33:23 +0100

    remove time for past events

--- packages/website/src/utils/index.ts
+++ packages/website/src/utils/index.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const formatTime = (value: number) =>
+  value < 10 ? `0${value}` : `${value}`;
