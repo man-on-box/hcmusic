ecba413cf7032d9a659a239c396f8e5b79337ebf
Oliver Hughes <oliver.hughes@pleo.io>
2026-04-15 19:05:45 +0100

    better formatting of dates

--- packages/website/src/utils/index.ts
+++ packages/website/src/utils/index.ts
@@ -5,5 +5,33 @@ export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export const formatTime = (value: number) =>
-  value < 10 ? `0${value}` : `${value}`;
+const formatter = new Intl.DateTimeFormat("en-GB", {
+  timeZone: "Europe/London",
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: false,
+});
+
+export const formatDate = (date: Date) => {
+  const parts = formatter.formatToParts(date);
+
+  const getPart = (type: keyof Intl.DateTimeFormatPartTypesRegistry) =>
+    parts.find((p) => p.type === type)?.value;
+
+  const day = getPart("day") || "";
+  const month = getPart("month") || "";
+  const hour = getPart("hour") || "";
+  const minute = getPart("minute") || "";
+  const year = getPart("year") || "";
+
+  return {
+    day,
+    month,
+    hour,
+    minute,
+    year,
+  };
+};
